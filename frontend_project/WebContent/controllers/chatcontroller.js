80136546f98e934e810d91957d20b1437422cc69
Bhanoo Pratap Singh <bhanoosinghpacex1@gmail.com>
2019-01-14 14:27:17 +0530

    Uploaded
    Uploaded

--- frontend_project/WebContent/controllers/chatcontroller.js
+++ frontend_project/WebContent/controllers/chatcontroller.js
@@ -1,18 +1,26 @@
-/**
- * 
- */
+
 app.controller('ChatCtrl', function($scope,$rootScope,chatService,$http)
 {
 	$scope.messages;
-	chatService.receive().then(function(message){$scope.messages=message.data;});
 	$scope.message="";
 	$scope.max=250;
+	
+	
+	
+	chatService.receive().then(function(message){
+		$scope.messages=message.data;
+		
+		});
 
 	$scope.addMessage=function()
 	{
 		chatService.send($rootScope.loggedInUser.firstname+":" +$scope.message);
 		$scope.message="";
 		
+		chatService.receive().then(function(message){
+			$scope.messages=message.data;
+			
+			});
 	};
 
 	
